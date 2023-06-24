7eec793d1bef86b73e34cdda6858e96d67f6bbe1
manish <bmanish04@outlook.com>
2023-06-25 01:25:59 +0800

    cleaned up code

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-from reg import regex
-from tabulate import tabulate
+import re
 
-link = "https://sg.finance.yahoo.com/quote/BABA/balance-sheet?p=BABA"
+
+def main(link):
+    get_data(link)
+    text = read_file("baba1_files", "main_page.txt")
+    pattern = r'<div class="M\(0\) Whs\(n\) BdEnd Bdc\(\$seperatorColor\) D\(itb\)">(.*?)<script>'
+    regex = regex(pattern, text, "first_reg.txt")
+    text = read_file("baba1_files", "first_reg.txt")
+    soup = BeautifulSoup(text, "html.parser")
+    table = soup.findAll("span")
+    final = list()
+    lst = list()
+
+    for span in table:
+        if span.text[0].isalpha():
+            if span.text == "ttm":
+                lst.append(span.text)
+            else:
+                final.append(lst)
+                lst = [
+                    span.text,
+                ]
+        else:
+            lst.append(span.text)
+    final.append(lst)
+    final = final[1:]
+    write_file(str(final), "./files", "data.txt")
+
 
 def get_data(link):
     with sync_playwright() as p:
@@ -14,44 +39,44 @@ def get_data(link):
         browser.close()
     write_file(text, "baba1_files", "main_page.txt")
 
+
 def write_file(text, folder, filename):
     with open(f"./{folder}/{filename}", "w") as file:
         file.write(text)
+
 
 def read_file(folder, filename):
     with open(f"./{folder}/{filename}", "r") as file:
         text = file.read()
     return text
 
-get_data(link)
 
-text = read_file("baba1_files", "main_page.txt")
+def reorg(data):
+    x = len(data[0])
+    org = list()
+    for i in range(x - 1):
+        print(i)
+        dic = dict()
+        for y in data:
+            print(y)
+            try:
+                dic[y[0]] = y[i]
+            except IndexError:
+                dic[y[0]] = "N/A"
+        org.append(dic)
 
-pattern = r'<div class="M\(0\) Whs\(n\) BdEnd Bdc\(\$seperatorColor\) D\(itb\)">(.*?)<script>'
-regex = regex(pattern, text, "first_reg.txt")
 
-text = read_file("baba1_files", "first_reg.txt")
+def regex(pattern, text, filename):
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        extracted_text = match.group(1)
+        with open(filename, "w") as file:
+            file.write(extracted_text)
 
-soup = BeautifulSoup(text, "html.parser")
-
-table = soup.findAll("span")
-
-final = list()
-lst = list()
-for span in table:
-    if span.text[0].isalpha():
-        if span.text == "ttm":
-            lst.append(span.text)
-        else:
-            final.append(lst)
-            lst = [span.text, ]
     else:
-        lst.append(span.text)
-final.append(lst)
+        print("No match found!")
 
-final = final[1:]
 
-table = tabulate(final, headers='firstrow', tablefmt='fancy_grid')
-
-write_file(str(final), "./files", "data.txt")
-print(table)
+if __name__ == "__main__":
+    link = input("Enter the link: ")
+    main(link)
