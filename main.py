b26413962cc84680f4f42ab3e21562eae6c59391
Manish <bmanish04@outlook>
2023-08-26 19:03:19 +0800

    fixed bug where not all data is scraped. now uses javascript to scrape

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
+import subprocess
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import re
 import threading
 import time
-import json 
+import json
 
 
-def main(link, sub):
-    text = get_data(link)
-    pattern = r'<div class="M\(0\) Whs\(n\) BdEnd Bdc\(\$seperatorColor\) D\(itb\)">(.*?)<script>'
-    specific_text = regex(pattern, text)
-    soup = BeautifulSoup(specific_text, "html.parser")
+def scrape(sub, ticker):
+    command = ["node", "scraper.js", ticker, sub]
+    text = subprocess.run(command,capture_output=True ,check=True).stdout.decode("utf-8")
+    soup = BeautifulSoup(text, "html.parser")
     table = soup.findAll("span")
     final = list()
     lst = list()
-
     for span in table:
         if span.text[0].isalpha():
             if span.text == "ttm":
@@ -29,7 +28,7 @@ def main(link, sub):
     final.append(lst)
     final = final[1:]
     re_organised = reorg(final)[1:]
-    with open(f"./files/{ticker}_{sub}.json", "w") as file:
+    with open(f"./files/{ticker}_{sub}.json", "w") as file:    
         json.dump(re_organised, file, indent=4)
 
 
@@ -43,6 +42,7 @@ def get_data(link):
         browser.close()
     return text
 
+
 def reorg(data):
     x = len(data[0])
     org = list()
@@ -50,7 +50,10 @@ def reorg(data):
         dic = dict()
         for y in data:
             try:
-                dic[y[0]] = y[i]
+                if "/" not in y[i] and y[i].lstrip("-")[0].isdigit():
+                    dic[y[0]] = float(y[i].replace(",", ""))
+                else:
+                    dic[y[0]] = y[i]
             except IndexError:
                 dic[y[0]] = "N/A"
         org.append(dic)
@@ -68,35 +71,72 @@ def regex(pattern, text):
         raise Exception("DATA NOT FOUND")
 
 
+def get_market_capitalisation(link, sub):
+    div_id = "quote-summary"
+    # Using playwright to extract data
+    data = get_data(link)
+    soup = BeautifulSoup(data, "html.parser")
+    # Find the table of data needed
+    target_div = soup.find("div", {"id": div_id})
+    # Find all the rows within the table
+    table_rows = target_div.find_all("tr")
+
+    # Initialize an empty dictionary to store the data
+    table_data = {}
+
+    # Iterate through rows and extract data
+    for row in table_rows:
+        columns = row.find_all("td")
+        if len(columns) == 2:  # Ensure it's a data row
+            key = columns[0].get_text(strip=True)
+            value = columns[1].get_text(strip=True)
+            table_data[key] = value
+
+    # Save the extracted data as a JSON file
+    with open(f"./files/{ticker}_summary_data.json", "w") as json_file:
+        json.dump([table_data,], json_file, indent=4)
+
+
 if __name__ == "__main__":
     start = time.time()
-    ticker = input("Enter the ticker: ")
+    # ticker = input("Enter the ticker: ")
+    ticker = "AAPL"
     t1 = threading.Thread(
-        target=main,
+        target=scrape,
         args=(
-            f"https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}",
             "financials",
+            ticker,
         ),
     )
     t2 = threading.Thread(
-        target=main,
+        target=scrape,
         args=(
-            f"https://finance.yahoo.com/quote/{ticker}/cash-flow?p={ticker}",
             "cash-flow",
+            ticker,
         ),
     )
     t3 = threading.Thread(
-        target=main,
+        target=scrape,
         args=(
-            f"https://finance.yahoo.com/quote/{ticker}/balance-sheet?p={ticker}",
             "balance-sheet",
+            ticker,
+        ),
+    )
+    t4 = threading.Thread(
+        target=get_market_capitalisation,
+        args=(
+            f"https://finance.yahoo.com/quote/{ticker}?p={ticker}",
+            ticker,
         ),
     )
     t1.start()
     t2.start()
     t3.start()
+    t4.start()
     t1.join()
     t2.join()
     t3.join()
+    t4.join()
+    # scrape("balance-sheet", ticker)
     end = time.time()
     print(f"Done! only took {end-start} seconds")
